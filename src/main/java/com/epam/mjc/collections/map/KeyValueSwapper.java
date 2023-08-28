9caf46d1714480465aa01de81dd94b166c1bce80
JustDevWeb <zm91672@gmail.com>
2023-08-28 10:06:40 +0500

    stage1-module4-collections-task3-map done

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,22 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> swapedMap = new HashMap<>();
+
+        for ( Map.Entry<Integer, String> e : sourceMap.entrySet() ){
+            String key = e.getValue();
+            Integer value = e.getKey();
+
+            if( swapedMap.containsKey(key)  && swapedMap.get(key) > value  ) {
+                swapedMap.put( key , value);
+            }
+
+            swapedMap.put( key , value);
+        }
+        return swapedMap;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,35 @@
 package com.epam.mjc.collections.map;
 
-import java.util.Map;
+import java.util.*;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+
+        String[] words = splitString(sentence);
+
+        Map<String, Integer> hashMap = new HashMap<>();
+
+        for (int i = 0; i < words.length; i++) {
+            int counter = 1;
+            String word = words[i].toLowerCase();
+            for ( int j = i + 1; j < words.length; j++ ) {
+                if (word.equals(words[j].toLowerCase())) {
+                    counter++;
+                }
+            }
+            if( !hashMap.containsKey(word) ) {
+                hashMap.put(word,counter);
+            }
+        }
+
+        return hashMap;
+    }
+
+    public String[] splitString (String sentence) {
+        String[] words = sentence.split("[ .,]");
+        List<String> wordsList = new ArrayList<>(List.of(words));
+        wordsList.removeAll(List.of(""));
+
+        return wordsList.toArray(new String[0]);
     }
 }
